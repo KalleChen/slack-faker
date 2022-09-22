874c26a0d5fe47742f3ea7dba5e6e50f2464f470
Kalle Chen <kallechen.tw@gmail.com>
2022-09-22 16:30:15 +0800

    chore: add tailwindCss

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,9 @@
+import './index.css'
+
 import React from 'react'
 
 const App = () => {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  )
+  return <div className="text-5xl font-bold text-center mt-4">Slack Faker</div>
 }
 
 export default App
