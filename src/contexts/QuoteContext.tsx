ac59cc5c76f9c23764dac5cb191dfd52c68f84d5
Kalle Chen <kallechen.tw@gmail.com>
2022-09-25 16:47:11 +0800

    feat: add emoji list

--- src/contexts/QuoteContext.tsx
+++ src/contexts/QuoteContext.tsx
@@ -7,27 +7,52 @@ interface Props {
 export interface QuoteContextInterface {
   name: string
   handleChangeName: (e: React.FormEvent<HTMLInputElement>) => void
-  image: File | null
-  handleChangeImage: (e: React.FormEvent<HTMLInputElement>) => void
+  profile: File | null
+  handleChangeProfile: (e: React.FormEvent<HTMLInputElement>) => void
   time: string
   handleChangeTime: (e: React.FormEvent<HTMLInputElement>) => void
+  emojiList: Array<File | boolean>
+  handleChangeEmojiList: (
+    e: React.FormEvent<HTMLInputElement>,
+    index: number
+  ) => void
+  emojiNUmberList: string[]
+  handleChangeEmojiNumberList: (
+    e: React.FormEvent<HTMLInputElement>,
+    index: number
+  ) => void
+  handleAddEmoji: () => void
+  replyNUmber: string
+  handleChangeReplyNumber: (e: React.FormEvent<HTMLInputElement>) => void
 }
 
 const initialValue: QuoteContextInterface = {
   name: '',
   handleChangeName: () => {},
-  image: null,
-  handleChangeImage: () => {},
+  profile: null,
+  handleChangeProfile: () => {},
   time: '00:00:00',
   handleChangeTime: () => {},
+  emojiList: [],
+  handleChangeEmojiList: () => {},
+  emojiNUmberList: [],
+  handleChangeEmojiNumberList: () => {},
+  handleAddEmoji: () => {},
+  replyNUmber: '0',
+  handleChangeReplyNumber: () => {},
 }
 
 const QuoteContext = createContext<QuoteContextInterface>(initialValue)
 
 export const QuoteContextProvider: React.FC<Props> = ({ children }) => {
   const [name, setName] = useState(initialValue.name)
-  const [image, setImage] = useState(initialValue.image)
+  const [profile, setProfile] = useState(initialValue.profile)
   const [time, setTime] = useState(initialValue.time)
+  const [emojiList, setEmojiList] = useState(initialValue.emojiList)
+  const [emojiNUmberList, setEmojiNUmberList] = useState(
+    initialValue.emojiNUmberList
+  )
+  const [replyNUmber, setReplyNUmber] = useState(initialValue.replyNUmber)
 
   const handleChangeName = useCallback(
     (e: React.FormEvent<HTMLInputElement>) => {
@@ -35,26 +60,64 @@ export const QuoteContextProvider: React.FC<Props> = ({ children }) => {
     },
     []
   )
-  const handleChangeImage = useCallback(
+  const handleChangeProfile = useCallback(
     (e: React.FormEvent<HTMLInputElement>) => {
-      setImage(e?.currentTarget?.files?.[0] ?? null)
+      setProfile(e?.currentTarget?.files?.[0] ?? null)
     },
     []
   )
+  const handleChangeEmojiList = useCallback(
+    (e: React.FormEvent<HTMLInputElement>, index: number) => {
+      const file = e?.currentTarget?.files?.[0] ?? false
+      setEmojiList((prev) => {
+        const newEmojiList = [...prev]
+        newEmojiList[index] = file
+        return newEmojiList
+      })
+    },
+    []
+  )
+  const handleChangeEmojiNumberList = useCallback(
+    (e: React.FormEvent<HTMLInputElement>, index: number) => {
+      const value = e?.currentTarget?.value || '0'
+      setEmojiNUmberList((prev) => {
+        const newEmojiNUmberList = [...prev]
+        newEmojiNUmberList[index] = value
+        return newEmojiNUmberList
+      })
+    },
+    []
+  )
+  const handleAddEmoji = useCallback(() => {
+    setEmojiList((prev) => [...prev, false])
+    setEmojiNUmberList((prev) => [...prev, '0'])
+  }, [])
   const handleChangeTime = useCallback(
     (e: React.FormEvent<HTMLInputElement>) => {
       setTime(e.currentTarget.value)
     },
     []
   )
-
+  const handleChangeReplyNumber = useCallback(
+    (e: React.FormEvent<HTMLInputElement>) => {
+      setReplyNUmber(e.currentTarget.value)
+    },
+    []
+  )
   const contextValues: QuoteContextInterface = {
     name,
-    image,
+    profile,
     handleChangeName,
-    handleChangeImage,
+    handleChangeProfile,
     time,
     handleChangeTime,
+    emojiList,
+    handleChangeEmojiList,
+    emojiNUmberList,
+    handleChangeEmojiNumberList,
+    replyNUmber,
+    handleChangeReplyNumber,
+    handleAddEmoji,
   }
 
   return (
--- src/layouts/Settings.tsx
+++ src/layouts/Settings.tsx
@@ -4,8 +4,19 @@ import Input from '../components/Input'
 import { useQuoteContext } from '../contexts/QuoteContext'
 
 const Settings: React.FC = () => {
-  const { name, handleChangeName, time, handleChangeTime, handleChangeImage } =
-    useQuoteContext()
+  const {
+    name,
+    handleChangeName,
+    time,
+    handleChangeTime,
+    handleChangeProfile,
+    handleChangeEmojiList,
+    emojiNUmberList,
+    handleChangeEmojiNumberList,
+    replyNUmber,
+    handleChangeReplyNumber,
+    handleAddEmoji,
+  } = useQuoteContext()
   return (
     <div className="grid grid-cols-2 p-6 w-full gap-4 max-w-[800px]">
       <Input
@@ -27,9 +38,42 @@ const Settings: React.FC = () => {
         placeholder=""
         value={undefined}
         type="file"
-        handleChange={handleChangeImage}
+        handleChange={handleChangeProfile}
         accept="image/*"
       />
+      <Input
+        label="reply number"
+        placeholder=""
+        value={replyNUmber}
+        type="number"
+        handleChange={handleChangeReplyNumber}
+      />
+      {emojiNUmberList.map((emojiNumber, index) => (
+        <React.Fragment key={index}>
+          <Input
+            label={`emoji picture ${index + 1}`}
+            placeholder=""
+            value={undefined}
+            type="file"
+            handleChange={(e) => handleChangeEmojiList(e, index)}
+            accept="image/*"
+          />
+          <Input
+            label="emoji number"
+            placeholder=""
+            value={emojiNumber}
+            type="number"
+            handleChange={(e) => handleChangeEmojiNumberList(e, index)}
+          />
+        </React.Fragment>
+      ))}
+      <button
+        type="button"
+        className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800 col-start-1 col-end-3"
+        onClick={handleAddEmoji}
+      >
+        Add Emoji
+      </button>
     </div>
   )
 }
