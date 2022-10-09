54be7fd5300529069e7e336564f99113058bbc7f
Kalle Chen <kallechen.tw@gmail.com>
2022-10-09 16:37:01 +0800

    feat: add github link

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,5 @@
 import './index.css'
 
-import React from 'react'
-
 import { QuoteContextProvider } from './contexts/QuoteContext'
 import Settings from './layouts/Settings'
 import SlackQuote from './layouts/SlackQuote'
@@ -13,6 +11,18 @@ const App = () => {
         <div className="text-5xl font-bold text-center mt-4 text-gray-300">
           Slack Faker
         </div>
+        <a
+          href="https://github.com/KalleChen/slack-faker"
+          target="_blank"
+          rel="noreferrer"
+          className="absolute top-10 right-10 w-12 h-12"
+        >
+          <img
+            src="https://cdn-icons-png.flaticon.com/512/25/25231.png"
+            alt="github-link"
+            className="right-4 w-12 h-12"
+          />
+        </a>
         <Settings />
         <SlackQuote />
       </div>
